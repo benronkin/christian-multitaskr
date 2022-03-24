c75b6f365ac327c952e417ab7e01b6f7e302b936
Ben Ronkin <ben@Bens-MacBook-Air.local>
2022-03-24 09:49:02 -0700

    v1 complete

--- Code.js
+++ Code.js
@@ -1,6 +1,144 @@
+/* global o _init DocumentApp _getFolderFilesIds CacheService GmailApp Logger SpreadsheetApp */
 /**
- *
+ * Shows messages with the given customer
+ * in the Easy to Read tab
+ * @param {*} email The customer email
  */
-const start = () => {
+// eslint-disable-next-line no-unused-vars
+const showMessages = (email) => {
+  if (!email) {
+    // user didn't select a customer. abort.
+    return;
+  }
+  // clear the panels from prior conversation
+  const cache = CacheService.getDocumentCache();
+  cache.remove('threadId');
+  cache.remove('threadSubject');
+  o.emailTemplateSelect.clearContent();
+  o.emailSubject.clearContent();
+  o.emailBody.clearContent();
+  o.emailMessage.setValue('');
+  o.threadSheet.getRange(4, 1, o.threadSheet.getLastRow(), 4).clearContent();
+  o.threadSheet.getRange(4, 6, o.threadSheet.getLastRow(), 5).clearContent();
+  let messageRows = o.logData
+    .filter((row) => [row[o.toCol], row[o.fromCol]].includes(email))
+    .sort((a, b) => new Date(a) - new Date(b));
+
+  if (messageRows.length == 0) {
+    return;
+  }
+  // cache the first thread subject so that it can be used
+  // to populate the subject field in the email sender
+  cache.put('threadId', messageRows[0][o.threadIdCol]);
+  cache.put('threadSubject', `Re: ${messageRows[0][o.subjectCol]}`);
+
+  const transcriptRows = messageRows.map((row) => {
+    const nr = new Array(4).fill('');
+    nr[0] = row[o.dateCol];
+    nr[1] = row[o.fromCol];
+    nr[2] = row[o.toCol];
+    nr[3] = row[o.bodyCol];
+    return nr;
+  });
+
+  messageRows = messageRows.map((row) => {
+    const nr = new Array(10).fill('');
+    if (row[o.fromCol] == email) {
+      nr[0] = row[o.dateCol];
+      nr[1] = row[o.bodyCol];
+      // nr[3] = o.clients[email];
+    } else {
+      const sender = row[o.fromCol];
+      nr[5] = row[o.bodyCol];
+      nr[8] = row[o.dateCol];
+      nr[9] = o.employees[sender] || sender;
+    }
+    return nr;
+  });
+  o.threadSheet.getRange(4, 1, messageRows.length, 10).setValues(messageRows);
+  o.transcriptSheet
+    .getRange(2, 1, o.transcriptSheet.getLastRow(), 4)
+    .clearContent();
+  o.transcriptSheet
+    .getRange(2, 1, transcriptRows.length, 4)
+    .setValues(transcriptRows);
+  SpreadsheetApp.flush();
+};
+
+/**
+ * Fetch the templates data from the Google Drive Folder
+ * @param {String} templateFolderId The id of the Google Drive folder containing the Google Doc templates
+ * @returns an object of the templates
+ */
+// eslint-disable-next-line no-unused-vars
+const _getTemplates = (templateFolderId) => {
+  const templates = {};
+  const fileIds = _getFolderFilesIds(templateFolderId);
+  fileIds.forEach((fileId) => {
+    const doc = DocumentApp.openById(fileId);
+    const body = doc.getBody();
+    const tables = body.getTables();
+    const templateName = doc.getName();
+    templates[templateName] = {
+      subject: tables[0].getText(),
+      body: tables[1].getText(),
+    };
+  });
+  return templates;
+};
+
+/**
+ * Populate subject and body of email client
+ * with template data
+ */
+// eslint-disable-next-line no-unused-vars
+const _populateEmailTemplate = (name) => {
+  let subject = '';
+  let body = '';
+  if (name) {
+    const cache = CacheService.getDocumentCache();
+    const templates = JSON.parse(cache.get('templates'));
+    const template = templates[name];
+    subject = cache.get('threadSubject') || template.subject;
+    body = template.body;
+  }
+  o.emailSubject.setValue(subject);
+  o.emailBody.setValue(body);
+};
+
+/**
+ * Send an email based on the data in the
+ * spreadsheet email client
+ */
+// eslint-disable-next-line no-unused-vars
+const sendEmail = () => {
   _init();
+  const body = o.emailBody.getValue();
+  const cache = CacheService.getDocumentCache();
+  const threadId = cache.get('threadId');
+  if (threadId) {
+    Logger.log('replied');
+    const thread = GmailApp.getThreadById(threadId);
+    thread.replyAll(body, { htmlBody: body });
+  } else {
+    Logger.log('emailed');
+    const subject = o.emailSubject.getValue();
+    const to = o.clients[o.threadDealSelector.getValue()];
+    const ccRowIdx =
+      o.clientSheet
+        .getDataRange()
+        .getValues()
+        .findIndex((row) => row[0] == to) + 1;
+    const cc = o.clientSheet
+      .getRange(ccRowIdx, 7, 1, o.clientSheet.getLastColumn())
+      .getValues()
+      .flat()
+      .filter(Boolean)
+      .join(',');
+    GmailApp.sendEmail(to, subject, body, { htmlBody: body, cc });
+  }
+  o.emailTemplateSelect.clearContent();
+  o.emailSubject.clearContent();
+  o.emailBody.clearContent();
+  o.emailMessage.setValue('Email Sent');
 };
--- Common.js
+++ Common.js
@@ -1,30 +1,138 @@
+/* global CacheService _getScriptProperty PropertiesService DriveApp Logger _getTemplates showMessages SpreadsheetApp _populateEmailTemplate */
 const o = {};
 
 /**
- * Intialize the automation
+ * Fetch the email templates.
+ * Fires via an installable trigger.
+ * @param {*} e
  */
+// eslint-disable-next-line no-unused-vars
+function onTriggeredOpen(e) {
+  _init();
+  const templateFolderId = _getScriptProperty('templateFolderId');
+  try {
+    DriveApp.getFolderById(templateFolderId);
+  } catch (e) {
+    Logger.log(
+      `Unable to locate the templates folder with id: ${templateFolderId}`
+    );
+    return;
+  }
+  const templates = _getTemplates(templateFolderId);
+  const rule = SpreadsheetApp.newDataValidation()
+    .requireValueInList(Object.keys(templates).sort())
+    .setAllowInvalid(false)
+    .setHelpText('Select a template name from the dropdown menu')
+    .build();
+  o.emailTemplateSelect.setDataValidation(rule);
+  const cache = CacheService.getDocumentCache();
+  cache.put('templates', JSON.stringify(templates), 21600);
+}
+
+/**
+ * Respond to spreadsheet edits
+ * @param {*} e The edit event object
+ */
+// eslint-disable-next-line no-unused-vars
+function onEdit(e) {
+  _init();
+  const sheet = e.source.getActiveSheet();
+  const thisRow = e.range.getRow();
+  const thisCol = e.range.getColumn();
+  const thisValue = e.range.getValue();
+  if (sheet.getName() == o.threadSheetName && thisCol == 4 && thisRow == 1) {
+    showMessages(o.clients[thisValue]);
+  }
+  if (
+    sheet.getName() == o.emailSheetName &&
+    thisRow == o.emailTemplateSelect.getRow() &&
+    thisCol == o.emailTemplateSelect.getColumn()
+  ) {
+    _populateEmailTemplate(thisValue);
+  }
+}
+
+/**
+ * Get the ids of all files inside a Google Drive folder
+ * @param {String} folderId The id of the folder containing the files
+ * @returns An array of file IDs
+ */
+// eslint-disable-next-line no-unused-vars
+const _getFolderFilesIds = (folderId) => {
+  const fileIds = [];
+  const files = DriveApp.searchFiles(`"${folderId}" in parents`);
+  while (files.hasNext()) {
+    const file = files.next();
+    fileIds.push(file.getId());
+  }
+  return fileIds;
+};
+
+/**
+ * Get the value of a given key in the Script properties
+ * @param {String} k the key to look for in ScriptProperty
+ * @returns the value associated with the key
+ */
+// eslint-disable-next-line no-unused-vars
+const _getScriptProperty = (k) => {
+  const scriptProperties = PropertiesService.getScriptProperties();
+  return scriptProperties.getProperty(k);
+};
+
+/**
+ * Set key and value in ScriptProperties
+ * @param {String} k the key to store in ScriptProperties
+ * @param {String} v the value to store in ScriptProperties
+ */
+// eslint-disable-next-line no-unused-vars
+const _setScriptProperty = (k, v) => {
+  const scriptProperties = PropertiesService.getScriptProperties();
+  const obj = {};
+  obj[k] = v;
+  scriptProperties.setProperties(obj);
+};
+
+/**
+ * Initialize the automation
+ */
+// eslint-disable-next-line no-unused-vars
 const _init = () => {
   o.ss = SpreadsheetApp.getActiveSpreadsheet();
-  o.adminSheet = o.ss.getSheetByName('Admin');
-  o.message = o.adminSheet.getRange('B6');
-  o.bgDefault = '#efefef';
-  o.bgWarning = '#ee9999';
-  o.bgSuccess = '#99ee99';
-  o.msgStart =
-    'Paste a client folder URL above, and select Create Tax Spreadsheet from the Tax Prep menu.';
-  o.message.setValue(o.msgStart);
-  o.message.setBackground(o.bgDefault);
+  o.threadSheetName = 'Easy Read';
+  o.threadSheet = o.ss.getSheetByName(o.threadSheetName);
+  o.threadDealSelector = o.threadSheet.getRange('D1');
+  o.logSheet = o.ss.getSheetByName('Log');
+  o.logData = o.logSheet.getDataRange().getValues();
+  o.emailSheetName = 'Send Email';
+  o.emailSheet = o.ss.getSheetByName(o.emailSheetName);
+  o.emailTemplateSelect = o.emailSheet.getRange('C2');
+  o.emailSubject = o.emailSheet.getRange('A2');
+  o.emailBody = o.emailSheet.getRange('A4');
+  o.emailMessage = o.emailSheet.getRange('A14');
+  o.transcriptSheetName = 'Transcript';
+  o.transcriptSheet = o.ss.getSheetByName(o.transcriptSheetName);
+  o.toCol = o.logData[0].indexOf('to');
+  o.fromCol = o.logData[0].indexOf('from');
+  o.dateCol = o.logData[0].indexOf('date');
+  o.bodyCol = o.logData[0].indexOf('body');
+  o.threadIdCol = o.logData[0].indexOf('threadId');
+  o.subjectCol = o.logData[0].indexOf('subject');
+  o.employeeSheetName = 'Employee Email to Name';
+  o.employeeSheet = o.ss.getSheetByName(o.employeeSheetName);
+  o.employees = {};
+  o.employeeSheet
+    .getDataRange()
+    .getValues()
+    .forEach((r) => (o.employees[r[0]] = r[1]));
+  o.clientSheetName = 'Email Addresses to Log';
+  o.clientSheet = o.ss.getSheetByName(o.clientSheetName);
+  o.clients = {};
+  o.clientSheet
+    .getDataRange()
+    .getValues()
+    .forEach((r) => (o.clients[r[1]] = r[0]));
+};
 
-  o.logHeaders = o.logSheet
-    .getRange(1, 1, 1, o.logSheet.getLastColumn())
-    .getVAlues()
-    .flat()
-    .map((x) => x.toLowerCase().trim());
-  o.configSheet = o.ss.getSheetByName('Config');
-  if (o.configSheet) {
-    o.configSheet
-      .getDataRange()
-      .getValues()
-      .forEach((row) => (o[row[0]] = row[1]));
-  }
+const dev = () => {
+  _setScriptProperty('templateFolderId', '1eT3Kiyh_g9WsZuma7taFNIYOkvSL7aJy');
 };
